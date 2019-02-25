6fa3a292e8e496f444d700324abf1ba564ce7ad9
Rachel <millerrach@users.noreply.github.com>
2019-02-25 13:25:54 +0100

    HondaExamples

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Intro from "./components/Intro.jsx";
 import WriteUp from "./components/WriteUp.jsx";
 import JuicelineExamples from './components/JuicelineExamples.jsx';
+import HondaExamples from './components/HondaExamples.jsx';
 
 class App extends Component {
   state = {
@@ -28,6 +29,7 @@ class App extends Component {
         <WriteUp width={width} index={0} short={true} />
         <JuicelineExamples width={width} />
         <WriteUp width={width} index={1} short={false} />
+        <HondaExamples width={width} />
       </div>
     );
   }
