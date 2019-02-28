a6b40174c25b72bd552dc8c09c978efe72b7835e
Rachel <millerrach@users.noreply.github.com>
2019-02-28 22:27:46 +0100

    Trailing animation for name and description

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import HondaExamples from './components/HondaExamples.jsx';
 import JubeckExamples from './components/JubeckExamples.jsx';
 
 const App = () => {
+
   function getSize() {
     return {
       innerHeight: window.innerHeight,
       innerWidth: window.innerWidth,
     };
   }
+
   let [windowSize, setWindowSize] = useState(getSize());
 
   function handleResize() {
@@ -24,15 +26,16 @@ const App = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
   const width = windowSize.innerWidth;
   return (
     <div className="App">
       <Intro width={width} />
-      <WriteUp width={width} index={0} short={true} />
+      <WriteUp index={0} />
       <JuicelineExamples width={width} />
-      <WriteUp width={width} index={1} short={false} />
+      <WriteUp width={width} index={1} />
       <HondaExamples width={width} />
-      <WriteUp width={width} index={2} short={false} />
+      <WriteUp width={width} index={2} />
       <JubeckExamples width={width} />
     </div>
   );
--- src/components/WriteUp.jsx
+++ src/components/WriteUp.jsx
@@ -1,25 +1,22 @@
 import React, { useState } from "react";
-import { useTrail, animated } from 'react-spring'
-
+import { Waypoint } from 'react-waypoint';
+import { useTrail, useSpring, animated } from 'react-spring'
 
 const writeUps = [
   {
     name: "Juiceline",
-    location: "Budapest, Hungary",
     description:
-      "A progressive web app that allows customers to see what's offered at the current moment.",
+      "Made for a juice company in Budapest, Hungary. A progressive web app that allows customers to see what's offered at the current moment.",
     stack: ["Figma", "React", "JS", "CSS3", "Sass"]
   },
   {
     name: "Honda",
-    location: "",
     description:
       "This project was done for Portlight Technologies. It’s a touch-screen app that ‘s displayed at Honda dealerships. It allows the user to view available makes, models, specs, colors, trim levels and current offers.",
     stack: ["JS", "HTML5", "CSS3", "Sass"]
   },
   {
     name: "Jubeck Brewery",
-    location: "",
     description:
       "This project was done for Jubeck New World Brewing. They were looking to track their beer memberships but couldn’t find a solution, so they came to us for a custom app. This allows them to add pints and growlers and also edit and renew memberships.",
     stack: ["React", "Redux", "MUI", "JS", "CSS3", "Sass"]
@@ -27,77 +24,75 @@ const writeUps = [
 ];
 
 const WriteUp = props => {
-  const { width, index, short } = props;
-  const sm = width < 600;
-  const areas = short ?
-    `
-        ".    name name name name name name name name stac stac .   "
-        ".    desc desc desc desc desc desc desc .    stac stac .   "` :
-
-    `
-        ".    name name name name name name name name .    .    .   "
-        ".    desc desc desc desc desc desc desc .    stac stac .   "`;
+  const [toggleStack, setStack] = useState(false);
+  const { index, bottomAlign } = props;
   const styles = {
     WriteUpStyle: {
       width: "100%",
       display: "grid",
       gridTemplateColumns: "repeat(12, 1fr)",
-      gridTemplateRows: "min-content 1fr",
-      gridTemplateAreas: areas,
+      gridTemplateAreas: `
+      ". nade nade nade nade nade nade nade . stac stac . "`,
       margin: "3em 0",
     },
-    nameLocation: {
-      gridArea: "name",
-      marginBottom: "1em",
+    nameDesc: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      gridArea: "nade",
     },
-    description: {
-      gridArea: "desc",
-    },
-    stackShort: {
+    stack: {
       gridArea: "stac",
       placeSelf: "end",
       lineHeight: "1.3"
     },
-    stackLong: {
-      gridArea: "stac",
-      alignSelf: "start",
-      justifySelf: "end",
-      lineHeight: "1.3"
-    }
   };
   const wui = writeUps[index];
-  const lengthStack = short ? styles.stackShort : styles.stackLong;
-  const items = wui.stack;
+  const skills = wui.stack;
+  const nameLocaDesc = [wui.name, wui.location, wui.description]
   const config = { mass: 10, tension: 1500, friction: 200 };
-  const [toggle, set] = useState(true)
-  const trail = useTrail(items.length, {
+  const trail = useTrail(skills.length, {
     config,
-    opacity: toggle ? 1 : 0,
-    x: toggle ? 0 : 20,
+    opacity: toggleStack ? 1 : 0,
+    x: toggleStack ? 0 : 20,
     from: { opacity: 0, x: 20 },
   })
+  const _toggle = () => {
+    setStack(true);
+  }
   return (
-    <div className="WriteUp" style={styles.WriteUpStyle}>
-      <div className="nameLocation" style={styles.nameLocation}>
-        <div className="name">{wui.name}</div>
-        <div className="location">{wui.location}</div>
-      </div>
-      <div className="description" style={styles.description}>{wui.description}</div>
-      <div className="stack" style={lengthStack}>
-        <div className="trails-main" onClick={() => set(state => !state)}>
-          <div>
-            {trail.map(({ x, ...rest }, index) => (
-              <animated.div
-                key={items[index]}
-                className="trails-text"
-                style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
-                <animated.div>{items[index]}</animated.div>
-              </animated.div>
-            ))}
-          </div>
+    <Waypoint onEnter={_toggle}>
+      <div className="WriteUp" style={styles.WriteUpStyle}>
+        <div className="nameDesc" style={styles.nameDesc}>
+          {trail.map(({ x, ...rest }, i) => (
+            <animated.div
+              key={i}
+              className="trails-text"
+              style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+              <animated.div>{nameLocaDesc[i]}</animated.div>
+            </animated.div>
+          ))}
+        </div>
+        {/* 
+        <div className="nameLocation" style={styles.nameLocation}>
+          <div className="name">{wui.name}</div>
+          <div className="location">{wui.location}</div>
+        </div>
+        <div className="description" style={styles.description}>
+          {wui.description}
+        </div> */}
+        <div className="stack" style={styles.stack}>
+          {trail.map(({ x, ...rest }, i) => (
+            <animated.div
+              key={skills[i]}
+              className="trails-text"
+              style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+              <animated.div>{skills[i]}</animated.div>
+            </animated.div>
+          ))}
         </div>
       </div>
-    </div>
+    </Waypoint>
   );
 }
 
